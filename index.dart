467d372e59bb36b9aaf36611f824426f0ee3f9bb
Prashant-2505 <102030344+Prashant-2505@users.noreply.github.com>
2022-03-22 11:42:16 +0530

    Create index.dart

---
+++ index.dart
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Parshant site</title>
+</head>
+
+<body>
+  <table cellspacing="20">
+    <tr>
+      <td>
+        <img src="images/Prashant.png" alt="Prashant profile" />
+
+      </td>
+      <td>
+        <p>
+        <h1>Prashant sharma</h1>
+        <em>Student of <strong>CSE</strong></em>
+        <em>Studying in
+          <strong><a href="http://www.hmritm.ac.in/">HMR institute of technology</a></strong></em>
+        </p>
+
+        <p>Hey i am just start learning HTML and i love coding</p>
+      </td>
+    </tr>
+
+  </table>
+
+
+
+
+  <hr>
+
+  <h3>Learning</h3>
+  <ul>
+    <li>c++</li>
+    <li>oops</li>
+    <li>dsa</li>
+    <li>HTML</li>
+  </ul>
+ 
+  <hr>
+
+  <h3>Experience</h3>
+
+  <table border="1">
+    <thead>
+      <th>Dates</th>
+      <th>Work</th>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>2015-2018</td>
+        <td>Diploma in Digital electronic</td>
+      </tr>
+      <tr>
+      <tr>
+        <td>2022-2024</td>
+        <td>B-tech in cse</td>
+      </tr>
+    </tbody>
+
+    <tfoot>
+
+
+    </tfoot>
+    </tr>
+  </table>
+
+<hr>
+
+  <h3>Skills</h3>
+  <table cellspacing="10">
+    <tr>
+      <td>c++ ⭐️⭐️⭐️⭐️</td>
+      <td> oops ⭐️⭐️⭐️</td>
+
+    </tr>
+
+    <tr>
+      <Td>dsa ⭐️ </Td>
+      <td> html ⭐️⭐️⭐️⭐️⭐️</td>
+
+    </tr>
+
+
+  </table>
+
+  <p><a href="Hobbies.html">Hobbies</a></p>
+  <p><a href="Contacts.html">Contacts me </a></p>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
